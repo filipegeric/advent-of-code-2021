0afc5d467f608ab85b9292abfbaf28a097511aef
Filip Egeric <filip.egeric@gmail.com>
2021-12-05 17:16:27 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        for (i in 1 until input.size) {
+            val previousValue = input[i - 1].toInt()
+            val currentValue = input[i].toInt()
+            if(currentValue > previousValue) count++
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        for(i in 3 until input.size) {
+            val previousValue = input[i - 3].toInt()
+            val currentValue = input[i].toInt()
+            if(currentValue > previousValue) count++
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
